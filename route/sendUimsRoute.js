3444c29ce769b8d0944a44ecae98c5c1f0011755
Ajuwon <ajuwonos1@gmail.com>
2026-08-25 15:52:03 +0100

    test

--- route/sendUimsRoute.js
+++ route/sendUimsRoute.js
@@ -3,6 +3,21 @@ import { sendUimsController } from "../controller/sendUimsController.js";
 
 const router = Router()
 
-router.get("/send_uims", sendUimsController)
+router.get("/send_uims",((req, res, next) => {
+  const originalEnd = res.end;
+
+  res.end = function (...args) {
+    console.log({
+      method: req.method,
+      url: req.originalUrl,
+      userAgent: req.get("user-agent"),
+      status: res.statusCode
+    });
+
+    return originalEnd.apply(this, args);
+  };
+
+  next();
+}), sendUimsController)
 
 export default router
