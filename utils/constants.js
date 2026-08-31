60b14a1bd42459839badb2d061c64b89efa7283d
Ajuwon <ajuwonos1@gmail.com>
2026-08-31 07:11:41 +0100

    Update how to use url

--- utils/constants.js
+++ utils/constants.js
@@ -7,7 +7,7 @@ export const API_KEY =
     : process.env.LIVE_SECRET_KEY;
 export const PAYMENT_URL = process.env.PAYMENT_INITIATION_URL;
 export const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
-export const HOW_TO_USE_URL = "google.com";
+export const HOW_TO_USE_URL = process.env.HOW_TO_USE_URL;
 export const PORT = parseInt(process.env.PORT);
 export const RESEND_API_KEY = process.env.RESEND_API_KEY;
 export const EMAIL = process.env.EMAIL;
